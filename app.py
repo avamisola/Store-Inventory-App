9d7bc83a6a17aa6b06afacfcdcb0052ad3314e73
avamisola <58097632+avamisola@users.noreply.github.com>
2020-09-02 19:46:40 -0400

    create Product model, inventory db

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 from peewee import *
 
+
 db = SqliteDatabase('inventory.db')
 
+
+class Product(Model):
+    product_id = AutoField()
+    product_name = CharField()
+    product_quantity = IntegerField()
+    product_price = DecimalField()
+    date_updated = DateField()
+
+    class Meta:
+        database = db
+        
+
+if __name__ == "__main__":
+    db.connect()
+    db.create_tables([Product], safe=True)
