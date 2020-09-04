291ab7a32bac94d40ff97989bdd14fe8b47d763c
avamisola <58097632+avamisola@users.noreply.github.com>
2020-09-04 12:40:53 -0400

    starting work on menu logic

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 
 from datetime import date
 from datetime import datetime
@@ -18,47 +19,97 @@ class Product(Model):
 
     class Meta:
         database = db
-        
 
-if __name__ == "__main__":
 
+def initialize():
+    """create database and table if don't exist"""
     db.connect()
     db.create_tables([Product], safe=True)
+
+
+def error_handler():
+    """generic error message and reroute to first menu choice"""
+    print("\n\nThat is not a valid option, please choose again.")
+    input("\nPress ENTER to continue...")
+    menu_loop()
+
+
+def menu_loop():
+    """show the menu"""
+    choice_main = None
+
+    print("""\n
+======================================================
+--Welcome to the Store Inventory Database--\n
+Select an option by entering the corresponding letter\n
+v) View entries in database
+a) Add entry to database
+b) Backup the database to csv
+q) Quit the app
+======================================================
+    """)
+
+    choice_main = input("Enter letter to select option: ").lower().strip()
+
+    if choice_main == 'q':
+        print("\n\nThank you for using this app. Goodbye!")
+        print("\n\n----END----\n\n")
+        sys.exit()
+    else:
+        error_handler
+        
+
+def view_entries():
+    """view entries in database"""
+
+
+def add_entry():
+    """add entry to database"""
+
+
+def backup_database():
+    """backup the database to csv"""
+
+
+def read_csv():
+    """read csv data into dictionary and clean data"""
+    with open('inventory.csv', newline='') as csvfile:
+        product_reader = csv.DictReader(csvfile, delimiter=',')
+        rows = list(product_reader)
+        product_list = []
+        for row in rows:
+            converted_price = float(row['product_price'].replace('$','')) * 100
+            row['product_price'] = round(converted_price)
+            converted_date = datetime.strptime(row['date_updated'], '%m/%d/%Y')
+            row['date_updated'] = datetime.combine(converted_date, datetime.min.time())
+            product_list.append(row)
+        return product_list
+
+
+def add_products():
+    """add cleaned data from csv to database"""
+    products = read_csv()
+    for product in products:
+        try:
+            Product.create(product_name=product['product_name'],
+                            product_price=product['product_price'],
+                            product_quantity=product['product_quantity'],
+                            date_updated=product['date_updated'])
+        except IntegrityError:
+            product_record = Product.get(product_name=product['product_name'])
+            product_record.product_price = product['product_price']
+            product_record.product_quantity = product['product_quantity']
+            product_record.date_updated = product['date_updated']
+            product_record.save()
+
+
+if __name__ == "__main__":
+    initialize()
+    add_products()
+    menu_loop()
 
 #    with open('requirements.txt', 'a') as csvfile:
 #        fieldnames = ['product_name','product_price','product_quantity','date_updated']
 #        productwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
 
-    def read_csv():
-        """read csv data into dictionary and clean data"""
-        with open('inventory.csv', newline='') as csvfile:
-            product_reader = csv.DictReader(csvfile, delimiter=',')
-            rows = list(product_reader)
-            product_list = []
-            for row in rows:
-                converted_price = float(row['product_price'].replace('$','')) * 100
-                row['product_price'] = round(converted_price)
-                converted_date = datetime.strptime(row['date_updated'], '%m/%d/%Y')
-                row['date_updated'] = datetime.combine(converted_date, datetime.min.time())
-                product_list.append(row)
-            return product_list
-
-
-    def add_products():
-        """add cleaned data from csv to database"""
-        products = read_csv()
-        for product in products:
-            try:
-                Product.create(product_name=product['product_name'],
-                                product_price=product['product_price'],
-                                product_quantity=product['product_quantity'],
-                                date_updated=product['date_updated'])
-            except IntegrityError:
-                product_record = Product.get(product_name=product['product_name'])
-                product_record.product_price = product['product_price']
-                product_record.product_quantity = product['product_quantity']
-                product_record.date_updated = product['date_updated']
-                product_record.save()
-
-    add_products()
